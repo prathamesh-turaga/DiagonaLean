2dd7a8670b249dd29ec3699eeddbf126df0b6877
Akhilesh Balaji <akhilesh.balaji.bangalore@gmail.com>
2026-08-14 14:19:44 +0530

    chore(): correct authorship in UTMHalt

--- DiagonaLean/UTMHalt/Reductions/Halt_to_UTMHalt.lean
+++ DiagonaLean/UTMHalt/Reductions/Halt_to_UTMHalt.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2026 Aalok Thakkar. All rights reserved.
+Copyright (c) 2026 Akhilesh Balaji. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Aristotle (Harmonic)
 -/
